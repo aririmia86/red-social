42bf8303fe7505ac689ac74f62c7989fe396a4a9
Alejandro Irimia <rimax_86@hotmail.com>
2018-03-05 19:19:42 +0100

    Registro de usuarios en el backend

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const bcrypt = require('bcrypt-nodejs');
 const userModel = require('../models/user');
 
 function home(req, res) {
@@ -8,6 +9,47 @@ function home(req, res) {
     });
 }
 
+function saveUser(req, res) {
+    const params = req.body;
+    const user = new userModel();
+    if (params.name
+        && params.surname
+        && params.nick
+        && params.email
+        && params.password) {
+        user.name = params.name;
+        user.surname = params.surname;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.role = 'ROLE_USER';
+        user.image = null;
+        bcrypt.hash(params.password, null, null, (err, hash) => {
+            user.password = hash;
+            user.save((err, userStored) => {
+                if (err) {
+                    return res.status(500).send({
+                        message: 'Error: Something went wrong'
+                    });
+                }
+                if (userStored) {
+                    res.status(200).send({
+                        user: userStored
+                    });
+                } else {
+                    res.status(404).send({
+                        message: 'Error: User not registered'
+                    });
+                }
+            });
+        });
+    } else {
+        res.status(400).send({
+            message: 'All data is required'
+        });
+    }
+}
+
 module.exports = {
-    home: home
+    home: home,
+    saveUser: saveUser
 };
--- api/routes/user.js
+++ api/routes/user.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/user');
 const api = express.Router();
 
 api.get('/home', userController.home);
+api.post('/register', userController.saveUser);
 
 module.exports = api;
