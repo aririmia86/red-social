9716307a0e09b5cfb1c0db45bb40adc6113234cc
Alejandro Irimia <rimax_86@hotmail.com>
2018-03-11 18:10:28 +0100

    Servicio y tokens JWT

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,6 +2,7 @@
 
 const bcrypt = require('bcrypt-nodejs');
 const userModel = require('../models/user');
+const jwt = require('../services/jwt');
 
 function home(req, res) {
     res.status(200).send({
@@ -83,8 +84,14 @@ function loginUser(req, res) {
         if (user) {
             bcrypt.compare(password, user.password, (err, check) => {
                if (check) {
-                   user.password = undefined;
-                   return res.status(200).send({user});
+                   if (params.getToken) {
+                       return res.status(200).send({
+                           token: jwt.createToken(user)
+                       });
+                   } else {
+                       user.password = undefined;
+                       return res.status(200).send({user});
+                   }
                } else {
                    return res.status(404).send({
                        message: 'User not identified'
